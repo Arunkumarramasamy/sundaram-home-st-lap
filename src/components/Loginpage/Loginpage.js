0455055756fd2fee28153993a7256687d1a8d9c3
Naveen <naveennsanthi@gmail.com>
2022-11-25 19:08:26 +0530

    login screen changes commit

--- src/components/Loginpage/Loginpage.js
+++ src/components/Loginpage/Loginpage.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import SfTeam from '../../images/sundaramteam.png';
-import SfLogo from '../../images/SF_Logo.png';
-import LoginImg from '../../images/login.jpg';
-import { Container } from '@mui/material';
-import { isMobile } from 'react-device-detect';
-import { InputAdornment, Paper } from '@material-ui/core';
-import { LoginOutlined, PasswordOutlined } from '@mui/icons-material';
-import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
-import PageLayout from '../Pagelayout/Pagelayout';
-import Cookies from 'js-cookie';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import SfTeam from "../../images/sundaramteam.png";
+import SfLogo from "../../images/SF_Logo.png";
+import LoginImg from "../../images/login.jpg";
+import { Container } from "@mui/material";
+import { isMobile } from "react-device-detect";
+import { InputAdornment, Paper } from "@material-ui/core";
+import { LoginOutlined, PasswordOutlined } from "@mui/icons-material";
+import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
+import PersonIcon from "@mui/icons-material/Person";
+import PageLayout from "../Pagelayout/Pagelayout";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 function Copyright() {
   return (
-    <Typography variant="footer" color="text.secondary" sx={{ color: 'white' }}>
-      {'Copyright © Sundaram Home Finance Pvt Ltd '}
-      {' '}
-      {new Date().getFullYear()}
-      {'.'}
+    <Typography variant="footer" color="text.secondary" sx={{ color: "white" }}>
+      {"Copyright © Sundaram Home Finance Pvt Ltd "} {new Date().getFullYear()}
+      {"."}
     </Typography>
   );
 }
@@ -36,183 +35,241 @@ const theme = createTheme();
 const screenHeight = window.innerHeight;
 
 export default function Loginpage() {
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      mobile: data.get('mobile'),
-      password: data.get('password'),
+      mobile: data.get("mobile"),
+      password: data.get("password"),
     });
-    Cookies.set('islogin',true);
-    navigate('/stlap/home/dashboard')
+    Cookies.set("islogin", true);
+    navigate("/stlap/home/dashboard");
   };
 
-  return (
-    (Cookies.get('islogin') ? <PageLayout /> :
+  return Cookies.get("islogin") ? (
+    <PageLayout />
+  ) : (
     <React.Fragment>
-      <Box component="header" sx={{
-        py: 3, px: 2, mt: 'auto',
-        backgroundColor: '#004A92',
-        width: '100%',
-        backgroundImage: `url(${SfLogo})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'left'
-      }}
+      <Box
+        component="header"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: "auto",
+          backgroundColor: "#004A92",
+          width: "100%",
+          backgroundImage: `url(${SfLogo})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "left",
+        }}
       />
-      {isMobile ? <React.Fragment><ThemeProvider theme={theme}>
-        <Grid container component="main" sx={{
-          backgroundPosition: 'center',
-          width : '100%',
-        }}>
-          <Grid item component={Paper} elevation={1} square={true} sx={{
-            backgroundImage: 'linear-gradient(#019CAD,white,#4880EC,#004A92)',
-            backgroundRepeat: 'no-repeat', backgroundSize: 'cover',
-            width:'100%',
-          }}>
-            <ThemeProvider theme={theme}>
-              <Container component="main">
-                <CssBaseline />
-                <Box
-                  sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    height: screenHeight - 184,
-                    textAlign:'center'
-                  }}
-                >
-                  <Avatar sx={{ m: 1, bgcolor: '#2F7DC4', width: '50px', height: '50px' }}>
-                    <LoginOutlined />
-                  </Avatar>
-                  <Typography component="h1" variant="h5" sx={{ color: 'black' }}>
-                    Login
-                  </Typography>
-                  <Box component="form" validate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                    <Grid container spacing={1}>
-                      <Grid item xs={12}>
-                        <TextField
-                          required
-                          fullWidth
-                          id="mobile"
-                          label="Mobile Number"
-                          name="mobile"
-                          autoComplete="mobile"
-                          size="small"
-                          autoFocus
-                          sx={{ color: '#7F7F7F' }}
-                          InputProps={{
-                            startAdornment: (
-                              <InputAdornment position="start">
-                                <PhoneAndroidIcon />
-                              </InputAdornment>
-                            ),
-                          }}
-                        />
-                      </Grid>
-                      <Grid item xs={12}>
-                        <TextField
-                          required
-                          fullWidth
-                          name="password"
-                          label="Password"
-                          type="password"
-                          id="password"
-                          autoComplete="new-password"
-                          size="small"
-                          sx={{ color: '#7F7F7F' }}
-                          InputProps={{
-                            startAdornment: (
-                              <InputAdornment position="start">
-                                <PasswordOutlined />
-                              </InputAdornment>
-                            ),
-                          }}
-                        />
-                      </Grid>
-                    </Grid>
-                    <Button
-                      type="submit"
-                      variant="contained"
-                      sx={{ mt: 3, mb: 2 }}
-                      size="small"
+      {isMobile ? (
+        <React.Fragment>
+          <ThemeProvider theme={theme}>
+            <Grid
+              container
+              component="main"
+              sx={{
+                backgroundPosition: "center",
+                width: "100%",
+              }}
+            >
+              <Grid
+                item
+                component={Paper}
+                elevation={1}
+                square={true}
+                sx={{
+                  backgroundImage:
+                    "linear-gradient(#019CAD,white,#4880EC,#004A92)",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  width: "100%",
+                }}
+              >
+                <ThemeProvider theme={theme}>
+                  <Container component="main">
+                    <CssBaseline />
+                    <Box
+                      sx={{
+                        marginTop: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        height: screenHeight - 184,
+                        textAlign: "center",
+                      }}
                     >
-                      Submit
-                    </Button>
-                    <Grid item justifyContent="center">
-                      <Link href="/" variant="body2" sx={{ color: 'black' }}>
-                        <strong>Forgot password?</strong>
-                      </Link>
-                    </Grid>
-                    <Grid item justifyContent="center" sx={{ marginTop: 1 }}>
-                      <Link href="/stlap/signup" variant="body2" sx={{ color: 'black' }}>
-                        {<strong>Don't have an account? Sign Up</strong>}
-                      </Link>
-                    </Grid>
-                  </Box>
-                </Box>
-              </Container>
-            </ThemeProvider>
-          </Grid>
-        </Grid>
-      </ThemeProvider>
-      </React.Fragment> :
+                      <Avatar
+                        sx={{
+                          m: 1,
+                          bgcolor: "#2F7DC4",
+                          width: "50px",
+                          height: "50px",
+                        }}
+                      >
+                        <LoginOutlined />
+                      </Avatar>
+                      <Typography
+                        component="h1"
+                        variant="h5"
+                        sx={{ color: "black" }}
+                      >
+                        Login
+                      </Typography>
+                      <Box
+                        component="form"
+                        validate
+                        onSubmit={handleSubmit}
+                        sx={{ mt: 1 }}
+                      >
+                        <Grid container spacing={1}>
+                          <Grid item xs={12}>
+                            <TextField
+                              required
+                              fullWidth
+                              id="User"
+                              label="User ID"
+                              name="User ID"
+                              autoComplete="User"
+                              size="small"
+                              autoFocus
+                              sx={{ color: "#7F7F7F" }}
+                              InputProps={{
+                                startAdornment: (
+                                  <InputAdornment position="start">
+                                    <PersonIcon />
+                                  </InputAdornment>
+                                ),
+                              }}
+                            />
+                          </Grid>
+                          <Grid item xs={12}>
+                            <TextField
+                              required
+                              fullWidth
+                              name="password"
+                              label="Password"
+                              type="password"
+                              id="password"
+                              autoComplete="new-password"
+                              size="small"
+                              sx={{ color: "#7F7F7F" }}
+                              InputProps={{
+                                startAdornment: (
+                                  <InputAdornment position="start">
+                                    <PasswordOutlined />
+                                  </InputAdornment>
+                                ),
+                              }}
+                            />
+                          </Grid>
+                        </Grid>
+                        <Button
+                          type="submit"
+                          variant="contained"
+                          sx={{ mt: 3, mb: 2 }}
+                          size="small"
+                        >
+                          Login
+                        </Button>
+                      </Box>
+                    </Box>
+                  </Container>
+                </ThemeProvider>
+              </Grid>
+            </Grid>
+          </ThemeProvider>
+        </React.Fragment>
+      ) : (
         /* below for browser */
         <ThemeProvider theme={theme}>
-          <Grid container component="main" sx={{ width:'100%' }}>
+          <Grid container component="main" sx={{ width: "100%" }}>
             <CssBaseline />
-            <Grid item xs={false} sm={4} md={7}
+            <Grid
+              item
+              xs={false}
+              sm={4}
+              md={7}
               sx={{
                 backgroundImage: `url(${SfTeam})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                width:'50%',
-                height:'81.8vh'
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                width: "50%",
+                height: "81.8vh",
               }}
             ></Grid>
-            <Grid item xs={12} sm={8} md={5} square="true" sx={{
-              borderColor: '#AAAAAA',
-              backgroundImage: 'linear-gradient(#019CAD,white,#4880EC,#004A92)', borderRadius: '80%',
-              backgroundRepeat: 'no-repeat', backgroundSize: 'cover',width:'50%',height:'81.8vh'
-            }}>
+            <Grid
+              item
+              xs={12}
+              sm={8}
+              md={5}
+              square="true"
+              sx={{
+                borderColor: "#AAAAAA",
+                backgroundImage:
+                  "linear-gradient(90deg, rgba(94,188,207,1) 5%, rgba(0,0,0,0.01726628151260501) 100%)",
+                borderRadius: "80%",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                width: "50%",
+                height: "81.8vh",
+              }}
+            >
               <ThemeProvider theme={theme}>
                 <Container component="main" maxWidth="xs">
                   <CssBaseline />
                   <Box
                     sx={{
                       marginTop: 8,
-                      display: 'flex',
-                      flexDirection: 'column',
-                      alignItems: 'center',
-                      textAlign:'center'
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      textAlign: "center",
                     }}
                   >
-                    <Avatar sx={{ m: 1, bgcolor: '#2F7DC4', width: '50px', height: '50px' }}>
+                    <Avatar
+                      sx={{
+                        m: 1,
+                        bgcolor: "#2F7DC4",
+                        width: "50px",
+                        height: "50px",
+                      }}
+                    >
                       <LoginOutlined />
                     </Avatar>
-                    <Typography component="h1" variant="h5" sx={{ color: 'black' }}>
+                    <Typography
+                      component="h1"
+                      variant="h5"
+                      sx={{ color: "black" }}
+                    >
                       Login
                     </Typography>
-                    <Box component="form" validate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                    <Box
+                      component="form"
+                      validate
+                      onSubmit={handleSubmit}
+                      sx={{ mt: 1 }}
+                    >
                       <Grid container spacing={1}>
                         <Grid item xs={12}>
                           <TextField
                             required
                             fullWidth
                             id="mobile"
-                            label="Mobile Number"
+                            label="User ID"
                             name="mobile"
                             autoComplete="mobile"
                             size="small"
                             autoFocus
-                            sx={{ color: '#7F7F7F' }}
+                            sx={{ color: "#7F7F7F" }}
                             InputProps={{
                               startAdornment: (
                                 <InputAdornment position="start">
-                                  <PhoneAndroidIcon />
+                                  <PersonIcon />
                                 </InputAdornment>
                               ),
                             }}
@@ -228,7 +285,7 @@ const navigate = useNavigate();
                             id="password"
                             autoComplete="new-password"
                             size="small"
-                            sx={{ color: '#7F7F7F' }}
+                            sx={{ color: "#7F7F7F" }}
                             InputProps={{
                               startAdornment: (
                                 <InputAdornment position="start">
@@ -245,41 +302,31 @@ const navigate = useNavigate();
                         sx={{ mt: 3, mb: 2 }}
                         size="small"
                       >
-                        Submit
+                        Login
                       </Button>
-                      <Grid item justifyContent="center">
-                        <Link href="/" variant="body2" sx={{ color: 'black' }}>
-                          <strong>Forgot password?</strong>
-                        </Link>
-                      </Grid>
-                      <Grid item justifyContent="center" sx={{ marginTop: 1 }}>
-                        <Link href="/stlap/signup" variant="body2" sx={{ color: 'black' }}>
-                          {<strong>Don't have an account? Sign Up</strong>}
-                        </Link>
-                      </Grid>
                     </Box>
                   </Box>
                 </Container>
               </ThemeProvider>
             </Grid>
           </Grid>
-        </ThemeProvider>}
+        </ThemeProvider>
+      )}
       <Box
         component="footer"
         sx={{
           py: 3,
           px: 2,
-          mt: 'auto',
-          backgroundColor: '#004A92',
-          textAlign:'center'
+          mt: "auto",
+          backgroundColor: "#004A92",
+          textAlign: "center",
         }}
       >
         <Container maxWidth="sm">
           <Copyright />
         </Container>
       </Box>
-    </React.Fragment>)
-
+    </React.Fragment>
   );
 }
 
