4e6f14dbe08a86a2692ce3d0c9346d758c7c1059
Vikas <vikas.s>
2022-12-02 12:43:28 +0530

    dashboard initial commit

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,22 @@
 import React from "react";
-
+import { Grid } from "@mui/material";
+import Masonry from '@mui/lab/Masonry';
+import { DashboardContent } from "./DashboardContent";
+import MediaQuery from "react-responsive";
 
 
 export const Dashboard = (props) => {
   return (
-   <div><h2>Dashboard Content</h2></div>
+    <React.Fragment>
+      <MediaQuery query="(min-device-width: 1201px)">
+        <Masonry spacing={1} columns={{ xs: 12, sm: 8, md: 6, lg: 4, xl: 4 }} >
+          <DashboardContent />
+        </Masonry>
+      </MediaQuery>
+      <MediaQuery query="(max-device-width: 1200px)">
+        <Grid container spacing={1} columns={{ xs: 12, sm: 8, md: 6, lg: 4, xl: 4 }}><DashboardContent /></Grid>
+      </MediaQuery>
+    </React.Fragment>
   );
 };
 
