c167a5ef3505008eacfa83e7d5c160d3389be3fc
Arun <arunkumar.r3@ivtlinfoview.co.jp>
2022-12-19 10:50:05 +0530

    Component Commit

--- src/components/CustomComponents/CustomDropDown.js
+++ src/components/CustomComponents/CustomDropDown.js
@@ -3,7 +3,7 @@ import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 const CustomDropDown = (props) => {
 
     return (<><InputLabel required={props.required} >{props.label}</InputLabel><FormControl fullWidth  variant={props.variant}>
-    <Select onChange={props.onChange} displayEmpty={props.displayEmpty} value={props.value}  id={props.id} disabled={props.disabled}>
+    <Select onChange={props.onChange} onBlur={props.onChange} error={props.onChange} displayEmpty={props.displayEmpty} value={props.value}  id={props.id} disabled={props.disabled}>
                     <MenuItem value="">
                       <p className="placeHolder_text">{props.placeholder}</p>
                     </MenuItem>
--- src/components/CustomComponents/CustomTextField.js
+++ src/components/CustomComponents/CustomTextField.js
@@ -2,10 +2,11 @@ import { InputLabel, TextField } from "@mui/material";
 import "./Custom.css";
 
 const CustomTextField = (props) => {
+    
 
     return (<>
         <InputLabel required={props.required} >{props.label}</InputLabel>
-        <TextField  fullWidth  id={props.id}  variant={props.variant}  value={props.value}  type={props.type} placeholder={props.placeholder} disabled={props.disabled} onChange={props.onChange}/>
+        <TextField  fullWidth  id={props.id}  variant={props.variant}  value={props.value}  type={props.type} placeholder={props.placeholder} disabled={props.disabled} onChange={props.onChange} onBlur={props.onBlur} error={props.error}/>
     </>);
 
 };
